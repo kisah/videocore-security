868b014ef720ac3d2afe1adf434712a584a22801
kisah <zazza2008@yandex.ru>
2026-03-04 20:29:58 +0300

    Some code comments

--- main.c
+++ main.c
@@ -46,16 +46,23 @@ SECURE_FUNCTION void set_interrupt(int intno, bool enable, int core) {
 }
 
 SECURE_FUNCTION void c_init(void) {
+    // Exception vectors
     for(int i = 0; i < 32; i++)
         isr_vectors[i] = exception_ignore;
+
+    // Software interrupt 0 with secure bit set
     isr_vectors[32] = (irqType)((uint32_t)secure_software_interrupt | 1);
+
+    // Hadware interrupt 0 (timer 0)
     isr_vectors[64] = timer_isr;
 
+    // Configure the interrupt controller
     IC0_VADDR = (uint32_t)isr_vectors;
     IC1_VADDR = (uint32_t)isr_vectors;
 
     set_interrupt(0, true, 0);
 
+    // Enable a secure memory region
     SDSECEND0 = ((uint32_t)_secure_ram_end & 0x3FFFFFFF) - 1;
     SDSECSRT0 = ((uint32_t)_secure_ram_start & 0x3FFFFFFF) | 1;
 }
